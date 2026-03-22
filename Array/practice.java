a152a8949fed4effa03f72b6856e8553eabda3be
harsh06s <harshsharma062003@gmail.com>
2026-03-22 23:55:48 +0530

    problems on array problems

--- Array/practice.java
+++ Array/practice.java
@@ -22,13 +22,38 @@ public class practice{
 
       }
         } 
+
+
+
+
+
+        static int[] arrayRotations(int arr[], int k){
+            int n = arr.length;
+            int[] ans = new int [n];
+            k = k%n;
+            int j = 0 ;
+            for (int i=n-k; i>n; i--){
+                ans[j] = ans[i];
+                j++;
+
+            }
+            for (int i=0; i<n-k; i++){
+                ans[j] = ans[i];
+                j++;
+
+            }return ans;
+        }
     
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5};
-        reverseArray(arr);
+        // reverseArray(arr);
+        int t =3;
+        arrayRotations(arr, t);
         for (int i=0; i<arr.length; i++) {
             System.out.println(arr[i]);
         }
+
+
         
     }
 }
