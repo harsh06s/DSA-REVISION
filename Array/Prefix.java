7ff0bcdf0e2baa1ebc13868225f653b6d36c863d
harsh06s <harshsharma062003@gmail.com>
2026-04-01 23:55:45 +0530

    prefix sum on array

--- Array/Prefix.java
+++ Array/Prefix.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 public class Prefix{
 
 
@@ -14,21 +17,50 @@ public class Prefix{
 
     static int[] prefixArray(int[] a){
         int n = a.length;
-        int[] pref = new int[n];
-        pref[0] = a[0];
+        //int[] pref = new int[n];
+        
         int i =1;
     
         while(i<n){
-            pref[i] = pref[i-1] + a[i];
+            a[i] = a[i-1] + a[i];
             i++;
-        }return pref;
+        }return a;
     }
 
 
+    static int prefixSelectedSum(int l,int r, int[] a ){
+        //a[0] = 0;
+        while (l<r){
+            a[l] = a[l-1] + a[l];
+            l++;
+        }return a[l-1];
+    }
+
+
+
+    
+
+
 public static void main(String[] args) {
-    int[] arr = {2,3,4,5};
-    System.err.println("hello world");
-    int[] pref =prefixArray(arr);
-    printArray(pref);
+    int[] arr = {2,1,3,4,5};
+
+    Scanner sc = new Scanner(System.in);
+    
+    int prefSum []=prefixArray(arr);
+    System.out.println("Enter number of querries");
+    int q = sc.nextInt();
+
+    while (q-->0){
+        System.out.println("Enter L and R respectively");
+        int l= sc.nextInt();
+        int r = sc.nextInt();
+        int ans = prefSum[r] - prefSum[l-1];
+        System.out.println("SUM = "+ans);
+    }
+
+    System.out.println();
+
+
+
 }
 }
