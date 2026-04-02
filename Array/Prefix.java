9cad86142e05f132d2be25990492d94e867c7ee0
harsh06s <harshsharma062003@gmail.com>
2026-04-02 23:34:09 +0530

    prefix sum and suffix sum

--- Array/Prefix.java
+++ Array/Prefix.java
@@ -36,6 +36,35 @@ public class Prefix{
         }return a[l-1];
     }
 
+    static int findArraySum(int[] arr){
+        int totalSum =0;
+        for (int i = 0; i < arr.length; i++) {
+            totalSum += arr[i];
+            
+        } return totalSum;
+    }
+
+
+
+
+    static boolean equalSumPartition(int[] arr){
+        int totalSum = findArraySum(arr);
+        int n = arr.length ;
+        int prefSum = 0 ;
+        
+        for (int i = 0; i <n; i++) {
+            prefSum += arr[i];
+             int suffixSum = totalSum- prefSum;
+            if (prefSum == suffixSum) {
+                return true ;
+            }
+
+            
+        } return false ;
+
+
+    }
+
 
 
     
@@ -45,10 +74,10 @@ public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the size of array");
     int n = sc.nextInt(); 
-    int[] arr = new int[n + 1];
+    int[] arr = new int[n];
     System.out.println("Enter " +n +" elements in array");
 
-    for (int i = 1; i <=n; i++) {
+    for (int i = 0; i<n; i++) {
         arr[i] = sc.nextInt();
         
     }
@@ -56,19 +85,19 @@ public static void main(String[] args) {
 
     
     
-    int prefSum []=prefixArray(arr);
-    System.out.println("Enter number of querries");
-    int q = sc.nextInt();
+    // int prefSum []=prefixArray(arr);
+    // System.out.println("Enter number of querries");
+    // int q = sc.nextInt();
 
-    while (q-->0){
-        System.out.println("Enter L and R respectively");
-        int l= sc.nextInt();
-        int r = sc.nextInt();
-        int ans = prefSum[r] - prefSum[l-1];
-        System.out.println("SUM = "+ans);
-    }
+    // while (q-->0){
+    //     System.out.println("Enter L and R respectively");
+    //     int l= sc.nextInt();
+    //     int r = sc.nextInt();
+    //     int ans = prefSum[r] - prefSum[l-1];
+    //     System.out.println("SUM = "+ans);
+    // }
 
-    System.out.println();
+    System.out.println("Equal partition possible "+ equalSumPartition(arr));
 
 
 
