ea2687e3ff441d42235c4ec2332ef8b3cd068ec7
harsh06s <harshsharma062003@gmail.com>
2026-04-03 23:31:53 +0530

    multi dimensional Array

---
+++ Array/Matrix.java
@@ -0,0 +1,29 @@
+
+
+public class Matrix{
+
+    static void printMatrix(int[][] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j]+" ");
+                
+            }System.err.println("");
+            
+        }
+    }
+
+
+    static int [][] matrixSum(int[][], int [][]){
+
+
+    }
+
+
+
+
+    public static void main(String [] args ){
+        int a [][] ={{1,2,3}, {2,3,4}};
+        printArray(a);
+
+    }
+}
